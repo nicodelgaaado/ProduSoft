cccab3368ab12d1d79d4b685c59c3caaecb65307
nicolas <nicolas.delgadop@campusuuc.edu.co>
2025-10-08 22:13:18 -0500

    Next.js Warning Fix

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,7 @@
 ﻿import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  outputFileTracingRoot: "C:/Users/Nlicolas/ProduSoft",
 };
 
 export default nextConfig;
